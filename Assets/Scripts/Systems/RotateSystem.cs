f77115289e23bf8cd870af403ef29d50bc1e1823
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2023-04-13 00:55:53 +0700

    minor edits in project

--- Assets/Scripts/Systems/RotateSystem.cs
+++ Assets/Scripts/Systems/RotateSystem.cs
@@ -6,6 +6,7 @@ namespace Systems
     public sealed class RotateSystem : IEcsRunSystem
     {
         private readonly EcsFilter<CharacterComponent, DirectionComponent, RotateComponent> _characterFilter;
+        private readonly EcsFilter<PersonComponent, RotateComponent>.Exclude<CharacterComponent> _personFilter;
 
         public void Run()
         {
@@ -14,9 +15,14 @@ namespace Systems
                 var directionX = _characterFilter.Get2(i).Direction.x;
 
                 if (directionX == 0f)
-                    return;
+                    continue;
 
                 _characterFilter.Get3(i).Renderer.flipX = directionX < 0f;
+            }
+
+            foreach (var i in _personFilter)
+            {
+
             }
         }
     }
