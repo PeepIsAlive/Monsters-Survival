95d0bf28652d60ffee422c8c000dcc0e816e2f36
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2023-05-20 02:01:17 +0700

    добавил кэширование для SettingsProvider'a

--- Assets/Scripts/Settings/SettingsProvider.cs
+++ Assets/Scripts/Settings/SettingsProvider.cs
@@ -1,14 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Settings
 {
     public static class SettingsProvider
     {
+        private static Dictionary<Type, ScriptableObject> _settings;
         private const string SETTINGS_PATH = "Settings/{0}";
+
+        static SettingsProvider()
+        {
+            _settings = new Dictionary<Type, ScriptableObject>();
+        }
 
         public static T Load<T>() where T : ScriptableObject
         {
-            return Resources.Load<T>(string.Format(SETTINGS_PATH, typeof(T).Name));
+            var type = typeof(T);
+
+            if (_settings.ContainsKey(type))
+                return (T)_settings[type];
+
+            var path = string.Format(SETTINGS_PATH, type.Name);
+            var so = Resources.Load<T>(path);
+
+            _settings.Add(type, so);
+
+            return so;
         }
     }
 }
