5f6a256b5284d35b9ae3454b920c4d32efef720a
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2023-05-20 00:43:53 +0700

    minor edits in Application.cs

--- Assets/Scripts/MonstersSurvival/Application.cs
+++ Assets/Scripts/MonstersSurvival/Application.cs
@@ -5,23 +5,12 @@ namespace MonstersSurvival
     public static class Application
     {
         public static Model Model { get; private set; }
-
-        public static RectTransform MainCanvasRect
-        {
-            get
-            {
-                if (_mainCanvasRect == null)
-                    _mainCanvasRect = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<RectTransform>();
-
-                return _mainCanvasRect;
-            }
-        }
-
-        private static RectTransform _mainCanvasRect;
+        public static RectTransform MainCanvasRect { get; private set; }
 
         static Application()
         {
             Model = new Model();
+            MainCanvasRect = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<RectTransform>();
         }
     }
 }
