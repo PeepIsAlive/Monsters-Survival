d0b23c732e7c7579b4efeaf025195b8b54a3cb2c
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2023-05-19 23:58:48 +0700

    Test system

---
+++ Assets/Scripts/Systems/TestSystem.cs
@@ -0,0 +1,38 @@
+#if UNITY_EDITOR
+using Leopotam.Ecs;
+using Components;
+using UnityEngine;
+using System;
+using UI;
+
+namespace Systems
+{
+    public sealed class TestSystem : IEcsRunSystem
+    {
+        private readonly EcsWorld _world;
+
+        public void Run()
+        {
+            PerformAction(KeyCode.U, KeyCode.I, () =>
+            {
+                _world.NewEntity().Replace(new ShowPopupComponent
+                {
+                    PopupToShow = new PopupToShow<DefaultPopup>(new DefaultPopup
+                    {
+
+                    })
+                });
+            });
+        }
+
+        private void PerformAction(KeyCode keyPress, KeyCode keyUp, Action action)
+        {
+            if (!Input.GetKey(keyPress))
+                return;
+
+            if (Input.GetKeyUp(keyUp))
+                action?.Invoke();
+        }
+    }
+}
+#endif
